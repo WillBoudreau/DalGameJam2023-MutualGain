b1d4f9ef351ce0e87c5a8ea2e4ab415d7cc487af
mFriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2023-11-11 17:40:37 -0400

    rubber duck where

---
+++ Assets/Scripts/LFRubberDuck.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+// This makes our list of ints that we choose from when randomizing things.
+{
+    int tempPlayers = 4;
+    int tempTradeCardLimit;
+
+    List<int> ints = new List<int>();
+
+    for (int i = 0; i < tempPlayers; i++)
+    {
+        ints.Add(i);
+    }
+}
