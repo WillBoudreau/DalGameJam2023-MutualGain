13d66a06534c02147db53ec09339db293b7481d9
mFriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2023-11-11 15:53:45 -0400

    Add some probably required vars.

--- Assets/Scripts/TradeLog.cs
+++ Assets/Scripts/TradeLog.cs
@@ -10,15 +10,23 @@ namespace Assets.Scripts
 {
     internal class TradeLog
     {
-        Card?[,] tempCards;
-        Card? offer;
-        Card? req;
+        public Card?[,] tempCards;
+        public Card? offer;
+        public Card? req;
+        public int index;
+        public int offerIndex;
+        public int target;
+        public int reqIndex;
 
-        public TradeLog(Card?[,] tempCards, Card? offer, Card? req)
+        public TradeLog(Card?[,] tempCards, Card? offer, Card? req, int index, int offerIndex, int target, int reqIndex)
         {
             this.tempCards = tempCards;
             this.offer = offer;
             this.req = req;
+            this.index = index;
+            this.offerIndex = offerIndex;
+            this.target = target;
+            this.reqIndex = reqIndex;
         }
     }
 }
