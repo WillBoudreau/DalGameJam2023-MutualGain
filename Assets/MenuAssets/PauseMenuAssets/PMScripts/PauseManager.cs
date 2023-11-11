62f558e501d4f316586ea227d9451c0d61e6894d
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2023-11-11 12:44:24 -0400

    Created Pause Menu And Function

---
+++ Assets/MenuAssets/PauseMenuAssets/PMScripts/PauseManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    public bool GameIsPaused;
+    // Start is called before the first frame update
+    public GameObject PauseMenuUI;
+    public GameObject GameUI;
+    private float WaitTime = 0.5f;
+    public void EndGame()
+    {
+        Debug.Log("You pressed quit");
+        StartCoroutine(QuitGame());
+    }
+    private IEnumerator QuitGame()
+    {
+        yield return new WaitForSeconds(WaitTime);
+        Application.Quit();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused == true)
+            {
+                ResumeGame();
+            }
+            if(GameIsPaused == false)
+            {
+                PauseGame();
+            }
+        }
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        StartCoroutine(Resume());
+    }
+    private IEnumerator Resume()
+    {
+        yield return new WaitForSeconds(WaitTime);
+        GameIsPaused = false;
+        PauseMenuUI.SetActive(false);
+        GameUI.SetActive(true);
+    }
+    void PauseGame()
+    {
+        GameIsPaused = true;
+        GameUI.SetActive(false);
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        StartCoroutine(NextScene());
+    }
+    private IEnumerator NextScene()
+    {
+        yield return new WaitForSeconds(WaitTime);
+        SceneManager.LoadScene(0);
+    }
+}
