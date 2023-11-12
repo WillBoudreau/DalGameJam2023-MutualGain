4f132347617bddf2856f6754701b4b365a3114c3
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2023-11-11 20:32:21 -0400

    Updated table and chairs

--- Assets/UIAssets/UIScripts/PlayerTurnUIController.cs
+++ Assets/UIAssets/UIScripts/PlayerTurnUIController.cs
@@ -7,7 +7,6 @@ public class PlayerTurnUIController : MonoBehaviour
     public float buttonDelay = 0.5f;
     public GameObject TurnUI;
     public GameObject TradeMenu;
-    public GameObject ActionMenu;
     public void SetTrade()
     {
         StartCoroutine(StartTrade());
@@ -17,26 +16,6 @@ public class PlayerTurnUIController : MonoBehaviour
         yield return new WaitForSeconds(buttonDelay);
         TurnUI.SetActive(false);
         TradeMenu.SetActive(true);
-    }
-    public void SetAction()
-    {
-        StartCoroutine(StartAction());
-    }
-    private IEnumerator StartAction()
-    {
-        yield return new WaitForSeconds(buttonDelay);
-        TurnUI.SetActive(false);
-        ActionMenu.SetActive(true);
-    }
-    public void ActionBack()
-    {
-        StartCoroutine(BackAction());
-    }
-    private IEnumerator BackAction()
-    {
-        yield return new WaitForSeconds(buttonDelay);
-        TurnUI.SetActive(true);
-        ActionMenu.SetActive(false);
     }
     public void TradeBack()
     {
