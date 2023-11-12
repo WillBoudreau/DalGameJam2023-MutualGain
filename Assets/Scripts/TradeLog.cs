e9407ee9bb5b7211e081eaf5c982e1d3fb51a48e
mFriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2023-11-12 11:58:09 -0400

    Cleanup

--- Assets/Scripts/TradeLog.cs
+++ Assets/Scripts/TradeLog.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using static UnityEngine.GraphicsBuffer;
-
-namespace Assets.Scripts
+﻿namespace Assets.Scripts
 {
     internal class TradeLog
     {
