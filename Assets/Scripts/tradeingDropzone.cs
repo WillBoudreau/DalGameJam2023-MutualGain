01d52210d10957ff3be31e0e8d2659052d459293
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2023-12-09 19:37:42 -0400

    minor changes

--- Assets/Scripts/tradeingDropzone.cs
+++ Assets/Scripts/tradeingDropzone.cs
@@ -8,6 +8,5 @@ public class tradeingDropzone : MonoBehaviour, IDropHandler
 public void OnDrop(PointerEventData eventData)
     {
         Debug.Log(eventData.pointerDrag.GetComponent<Card>().faceName + " of " + eventData.pointerDrag.GetComponent<Card>().suit  +" was dropped onto " + gameObject.name);
-        eventData.pointerDrag.GetComponent<Draggable>().returnPatent = this.transform;
     }
 }
